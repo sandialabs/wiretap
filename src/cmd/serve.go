dd16d8c79a2666545937b09f11726698a2d78060
Han Dai <github@daihan.me>
2023-03-18 23:59:08 -0400

    properly integrate viper with cobra

--- src/cmd/serve.go
+++ src/cmd/serve.go
@@ -25,18 +25,8 @@ import (
 
 type serveCmdConfig struct {
 	configFile string
-	privateKey string
-	publicKey  string
-	endpoint   string
-	port       int
 	quiet      bool
 	debug      bool
-	allowedIPs []string
-	addr4      string
-	addr6      string
-	apiAddr    string
-	keepalive  int
-	mtu        int
 	logging    bool
 	logFile    string
 }
@@ -44,18 +34,8 @@ type serveCmdConfig struct {
 // Defaults for serve command.
 var serveCmd = serveCmdConfig{
 	configFile: "",
-	privateKey: "",
-	publicKey:  "",
-	endpoint:   Endpoint,
-	port:       Port,
 	quiet:      false,
 	debug:      false,
-	allowedIPs: []string{Subnet4.Addr().Next().Next().String() + "/32", Subnet6.Addr().Next().Next().String() + "/128"},
-	addr4:      Subnet4.Addr().Next().String() + "/32",
-	addr6:      Subnet6.Addr().Next().String() + "/128",
-	apiAddr:    ApiAddr.String(),
-	mtu:        1420,
-	keepalive:  Keepalive,
 	logging:    false,
 	logFile:    "wiretap.log",
 }
@@ -67,9 +47,6 @@ func init() {
 		Use:   "serve",
 		Short: "Listen and proxy traffic into target network",
 		Long:  `Listen and proxy traffic into target network`,
-		PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
-			return serveCmd.PreRun()
-		},
 		Run: func(cmd *cobra.Command, args []string) {
 			serveCmd.Run()
 		},
@@ -77,29 +54,43 @@ func init() {
 
 	rootCmd.AddCommand(cmd)
 
+	// Flags.
 	cmd.Flags().StringVarP(&serveCmd.configFile, "config-file", "f", serveCmd.configFile, "wireguard config file to read from")
 	cmd.Flags().BoolVarP(&serveCmd.quiet, "quiet", "q", serveCmd.quiet, "silence wiretap log messages")
 	cmd.Flags().BoolVarP(&serveCmd.debug, "debug", "d", serveCmd.debug, "enable wireguard log messages")
 	cmd.Flags().BoolVarP(&serveCmd.logging, "log", "l", serveCmd.logging, "enable logging to file")
 	cmd.Flags().StringVarP(&serveCmd.logFile, "log-file", "o", serveCmd.logFile, "write log to this filename")
 
-	// Deprecated flags, kept for backwards compatibility.
-	cmd.Flags().StringVarP(&serveCmd.privateKey, "private", "", serveCmd.privateKey, "wireguard private key")
-	cmd.Flags().StringVarP(&serveCmd.publicKey, "public", "", serveCmd.publicKey, "wireguard public key of remote peer")
-	cmd.Flags().StringVarP(&serveCmd.endpoint, "endpoint", "e", serveCmd.endpoint, "socket address of remote peer that server will connect to (example \"1.2.3.4:51820\")")
-	cmd.Flags().IntVarP(&serveCmd.port, "port", "p", serveCmd.port, "wireguard listener port")
-	cmd.Flags().StringSliceVarP(&serveCmd.allowedIPs, "allowed", "a", serveCmd.allowedIPs, "comma-separated list of CIDR IP ranges to associate with peer")
-	cmd.Flags().StringVarP(&serveCmd.addr4, "ipv4", "4", serveCmd.addr4, "virtual ipv4 address of wireguard interface")
-	cmd.Flags().StringVarP(&serveCmd.addr6, "ipv6", "6", serveCmd.addr6, "virtual ipv6 address of wireguard interface")
-	cmd.Flags().StringVarP(&serveCmd.apiAddr, "api", "0", serveCmd.apiAddr, "address of API service")
-	cmd.Flags().IntVarP(&serveCmd.keepalive, "keepalive", "k", serveCmd.keepalive, "tunnel keepalive in seconds")
-	cmd.Flags().IntVarP(&serveCmd.mtu, "mtu", "m", serveCmd.mtu, "tunnel MTU")
-
 	// Quiet and debug flags must be used independently.
 	cmd.MarkFlagsMutuallyExclusive("debug", "quiet")
 
+	// Deprecated flags, kept for backwards compatibility.
+	cmd.Flags().StringP("private", "", "", "wireguard private key")
+	cmd.Flags().StringP("public", "", "", "wireguard public key of remote peer")
+	cmd.Flags().StringP("endpoint", "e", Endpoint, "socket address of remote peer that server will connect to (example \"1.2.3.4:51820\")")
+	cmd.Flags().IntP("port", "p", Port, "wireguard listener port")
+	cmd.Flags().StringSliceP("allowed", "a", []string{Subnet4.Addr().Next().Next().String() + "/32", Subnet6.Addr().Next().Next().String() + "/128"}, "comma-separated list of CIDR IP ranges to associate with peer")
+	cmd.Flags().StringP("ipv4", "4", Subnet4.Addr().Next().String() + "/32", "virtual ipv4 address of wireguard interface")
+	cmd.Flags().StringP("ipv6", "6", Subnet6.Addr().Next().String() + "/128", "virtual ipv6 address of wireguard interface")
+	cmd.Flags().StringP("api", "0", ApiAddr.String(), "address of API service")
+	cmd.Flags().IntP("keepalive", "k", Keepalive, "tunnel keepalive in seconds")
+	cmd.Flags().IntP("mtu", "m", 1420, "tunnel MTU")
+
+	// Bind deprecated flags to viper.
+	viper.BindPFlag("Interface.privatekey", cmd.Flags().Lookup("private"))
+	viper.BindPFlag("Interface.port", cmd.Flags().Lookup("port"))
+	viper.BindPFlag("Interface.ipv4", cmd.Flags().Lookup("ipv4"))
+	viper.BindPFlag("Interface.ipv6", cmd.Flags().Lookup("ipv6"))
+	viper.BindPFlag("Interface.api", cmd.Flags().Lookup("api"))
+	viper.BindPFlag("Interface.mtu", cmd.Flags().Lookup("mtu"))
+	viper.BindPFlag("Peer.publickey", cmd.Flags().Lookup("public"))
+	viper.BindPFlag("Peer.endpoint", cmd.Flags().Lookup("endpoint"))
+	viper.BindPFlag("Peer.allowed", cmd.Flags().Lookup("allowed"))
+	viper.BindPFlag("Peer.keepalive", cmd.Flags().Lookup("keepalive"))
+
 	cmd.Flags().SortFlags = false
 
+	// Hide deprecated flags and log flags.
 	helpFunc := cmd.HelpFunc()
 	cmd.SetHelpFunc(func(cmd *cobra.Command, args []string) {
 		if !ShowHidden {
@@ -114,64 +105,22 @@ func init() {
 	})
 }
 
-// Checks for config file flag and reads config file if present.
-func (c serveCmdConfig) PreRun() error {
-	// Precedence: environment variable > config file > command line flag (deprecated) > default value
+// Run parses/processes/validates args and then connects to peer,
+// proxying traffic from peer into local network.
+func (c serveCmdConfig) Run() {
+	// Read config from file and/or environment.
 	viper.AutomaticEnv()
 	viper.SetEnvPrefix("WIRETAP")
 	viper.SetEnvKeyReplacer(strings.NewReplacer(".", "_"))
 
-	// Read config file if present.
-	if serveCmd.configFile != "" {
+	if c.configFile != "" {
 		viper.SetConfigType("ini")
-		viper.SetConfigFile(serveCmd.configFile)
+		viper.SetConfigFile(c.configFile)
 		if err := viper.ReadInConfig(); err != nil {
-			return err
+			check("error reading config file", err)
 		}
 	}
 
-	if viper.IsSet("Interface.private") {
-		serveCmd.privateKey = viper.GetString("Interface.private")
-	}
-	if viper.IsSet("Interface.port") {
-		serveCmd.port = viper.GetInt("Interface.port")
-	}
-	if viper.IsSet("Interface.ipv4") {
-		serveCmd.addr4 = viper.GetString("Interface.ipv4")
-	}
-	if viper.IsSet("Interface.ipv6") {
-		serveCmd.addr6 = viper.GetString("Interface.ipv6")
-	}
-	if viper.IsSet("Interface.api") {
-		serveCmd.apiAddr = viper.GetString("Interface.api")
-	}
-	if viper.IsSet("Interface.mtu") {
-		serveCmd.mtu = viper.GetInt("Interface.mtu")
-	}
-	if viper.IsSet("Peer.public") {
-		serveCmd.publicKey = viper.GetString("Peer.public")
-	}
-	if viper.IsSet("Peer.endpoint") {
-		serveCmd.endpoint = viper.GetString("Peer.endpoint")
-	}
-	if viper.IsSet("Peer.allowed") {
-		serveCmd.allowedIPs = strings.Split(viper.GetString("Peer.allowed"), ",")
-	}
-	if viper.IsSet("Peer.keepalive") {
-		serveCmd.keepalive = viper.GetInt("Peer.keepalive")
-	}
-
-
-	// Check for required flags.
-	if serveCmd.publicKey == "" {
-		return errors.New("Public key is required")
-	}
-	return nil
-}
-
-// Run parses/processes/validates args and then connects to peer,
-// proxying traffic from peer into local network.
-func (c serveCmdConfig) Run() {
 	// Synchronization vars.
 	var (
 		wg   sync.WaitGroup
@@ -196,18 +145,23 @@ func (c serveCmdConfig) Run() {
 		}
 	}
 
+	// Check for required flags.
+	if !viper.IsSet("Peer.publickey") {
+		check("Public key is required", errors.New("Public key is required"))
+	}
+
 	configArgs := peer.ConfigArgs{
-		PrivateKey: c.privateKey,
-		ListenPort: c.port,
+		PrivateKey: viper.GetString("Interface.privatekey"),
+		ListenPort: viper.GetInt("Interface.port"),
 		Peers: []peer.PeerConfigArgs{
 			{
-				PublicKey:                   c.publicKey,
-				Endpoint:                    c.endpoint,
-				PersistentKeepaliveInterval: c.keepalive,
-				AllowedIPs:                  c.allowedIPs,
+				PublicKey:                   viper.GetString("Peer.publickey"),
+				Endpoint:                    viper.GetString("Peer.endpoint"),
+				PersistentKeepaliveInterval: viper.GetInt("Peer.keepalive"),
+				AllowedIPs:                  strings.Split(viper.GetString("Peer.allowed"), ","),
 			},
 		},
-		Addresses: []string{c.addr4, c.addr6},
+		Addresses: []string{viper.GetString("Interface.ipv4"), viper.GetString("Interface.ipv6")},
 	}
 
 	config, err := peer.GetConfig(configArgs)
@@ -223,22 +177,22 @@ func (c serveCmdConfig) Run() {
 	fmt.Println()
 
 	// Create virtual interface with this address and MTU.
-	ipv4Prefix, err := netip.ParsePrefix(c.addr4)
+	ipv4Prefix, err := netip.ParsePrefix(viper.GetString("Interface.ipv4"))
 	check("failed to parse ipv4 address", err)
 	ipv4Addr := ipv4Prefix.Addr()
 
-	ipv6Prefix, err := netip.ParsePrefix(c.addr6)
+	ipv6Prefix, err := netip.ParsePrefix(viper.GetString("Interface.ipv6"))
 	check("failed to parse ipv6 address", err)
 	ipv6Addr := ipv6Prefix.Addr()
 
-	apiPrefix, err := netip.ParsePrefix(c.apiAddr)
+	apiPrefix, err := netip.ParsePrefix(viper.GetString("Interface.api"))
 	check("failed to parse API address", err)
 	apiAddr := apiPrefix.Addr()
 
 	tun, tnet, err := netstack.CreateNetTUN(
 		[]netip.Addr{ipv4Addr, ipv6Addr, apiAddr},
 		[]netip.Addr{},
-		c.mtu,
+		viper.GetInt("Interface.mtu"),
 	)
 	check("failed to create TUN", err)
 
